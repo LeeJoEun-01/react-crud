7c46db0cbeaa18c46000a7b4695c17677488f600
LeeJoEun-01 <joeun1005@naver.com>
2022-04-07 10:36:07 +0900

    Edit Project

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -6,19 +6,18 @@ export default function Update(props) {
   return (
     <article>
       <h2>Update</h2>
-      <form onSubmit={(event)=>{
-        event.preventDefault();
-        const title = event.target.title.value;
-        const body = event.target.body.vaule;
-        props.onUpdate(title, body);
-      }}>
-        {/* value={ title } update전의 원래 title 값 불러와서 보여줌 */}
-        <p><input type="text" name="title" placeholder="title" value={ title } onChange={(event)=>{
-          setTitle(event.target.value);
-        }}></input></p>
-        <p><textarea name="body" placeholder="body" value={ body } onChange={(event) => {
-          setBody(event.target.value);
-        }} ></textarea></p>
+      <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onUpdate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title" value={title} onChange={event=>{
+        setTitle(event.target.value);
+      }}/></p>
+      <p><textarea name="body" placeholder="body" value={body} onChange={event=>{
+        setBody(event.target.value);
+      }}></textarea></p>
         <p><input type="submit" value="Update"></input></p>
       </form>
     </article>
